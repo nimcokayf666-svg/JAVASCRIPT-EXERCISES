6185eed9d314f752be0710f4cfce231cd4f793bd
Nimco <nimcokayf666@gmail.com>
2026-09-14 19:50:39 +0300

    execise6 print

---
+++ execise 6/main.js
@@ -0,0 +1,134 @@
+// 1.student informatin
+function studen(id,name,age,email,faculty){
+
+    console.log(id,name,age,email,faculty)
+}
+
+studen("Aamin",40,"A@gemail.come","softwhere eng",1)
+
+// 2. teacher information
+
+function teacher(id,name,age,subject){
+    console.log(id,name,age,subject)
+}
+
+teacher("suuban",24,"wep",2)
+
+// 3.employee information
+
+function employee(id,name,age,email,department){
+    console.log(id,name,age,email,department)
+}
+
+employee("maxamed",26,"m@email.come","IT",3)
+
+// 4.product information
+
+function product(id,name,price,category){
+    console.log(id,name,price,category)
+
+}
+
+product("laptop",430,"electronic",6)
+
+
+ //  customer information
+function customer(id,name,phone,email){
+    console.log(id,name,phone,email)
+}
+
+customer("ayaan",907665544,"A@gemail.come",1);
+
+// book information
+
+function book(id,title,author,price,){
+    console.log(id,title,author,price,)
+}
+
+book("javascript","haybe",30,2)
+
+ // car information
+
+function car(brand,model,year,price){
+    console.log(brand,model,year,price)
+}
+
+car("tayota","corolla",2022,15000)
+
+ // course information
+
+function course(id,name,Teacher,duration){
+    console.log(id,name,Teacher,duration)
+}
+
+course("wepdesing","axmed","3month",4)
+
+ // hospital patient information
+
+function hospital(id,name,age,disease){
+    console.log(id,name,age,disease)
+}
+
+hospital("kayf",25,"fever",5)
+
+
+ // phone information
+
+function phone(brand,model,storage,price){
+    console.log(brand,model,storage,price)
+}
+
+phone("samsung","A05","128GB",150)
+
+
+ // 11.resturant food information
+
+function food(id,name,price,category){
+    console.log(id,name,price,category)
+}
+
+food("pizza",6,"fast food",505)
+
+// 12.university information
+
+function university(name,city,Students,departments){
+    console.log(name,city,Students,departments)
+}
+
+university("frontier","gorowe",5000,"IT")
+
+ // 13. bank Account information
+
+function bank(Accountnumber,name,blance){
+    console.log(Accountnumber,name,blance)
+}
+
+bank(12345667,"nimco",1000)
+
+ // 14. mobele app information
+
+function mobele(name,vertion,developer,category){
+    console.log(name,vertion,developer,category)
+}
+
+mobele("whatsup",2,"meta","social media")
+
+// // 15.student grade
+
+function grade(name,subject,mark){
+   return name+""+subject+""+mark
+}
+
+let result = grade("najax ","javascript ",85)
+console.log(result)
+
+// 16.student Array
+
+let Student =["sihaam","nimco","burhaan","dheeman","Aamin"]
+ console.log(Student)
+
+
+   
+
+
+
--- execise5/main.js
+++ execise5/main.js
@@ -1,32 +1,36 @@
-// 1. Student Information
-function Student(name,age,email,faculty,address,uneversty,schoolName){
+// // 1. Student Information
+// function Student(name,age,email,faculty,address,uneversty,schoolName){
 
-     console.log( name, age, email, faculty,address,uneversty,schoolName)
-}
+//      console.log( name, age, email, faculty,address,uneversty,schoolName)
+// }
 
-Student("nimco",22,"n@gemail.com","softwer eng",101,"frontier,gambol",1)
+// Student("nimco",22,"n@gemail.com","softwer eng",101,"frontier,gambol",1)
 
-// 2. Teacher Information
+// // 2. Teacher Information
 
-function Teacher(name, age, subject,adress,phone,email){
+// function Teacher(name, age, subject,adress,phone,email){
     
-    console.log(name, age, subject,adress,phone,email)
-}
+//     console.log(name, age, subject,adress,phone,email)
+// }
 
-Teacher("burhaan",24,"javascript",102,7564618,"b@gemaul.com",2)
+// Teacher("burhaan",24,"javascript",102,7564618,"b@gemaul.com",2)
 
-// 3. Product Information
+// // 3. Product Information
 
-function Product(name, price, category,adress,shop){
-    console.log(name, price, category,adress,shop)
-}
+// function Product(name, price, category,adress,shop){
+//     console.log(name, price, category,adress,shop)
+// }
 
-Product("laptop",500,"electronic","gorowe","nimco shop",3)
+// Product("laptop",500,"electronic","gorowe","nimco shop",3)
 
-// 4. Employee Information
+// // 4. Employee Information
 
-function Employee(name, age, email, department){
-    console.log(name, age, email, department)
-}
+// function Employee(name, age, email, department){
+//     console.log(name, age, email, department)
+// }
 
-Employee("sihaam",22,"s@gemail.com","IT",4)
+// Employee("sihaam",22,"s@gemail.com","IT",4)
+
+
+
+
