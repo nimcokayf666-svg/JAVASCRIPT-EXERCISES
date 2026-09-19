4512eac0de4834eadb840883c421d40082d2dc34
Nimco <nimcokayf666@gmail.com>
2026-09-19 09:44:38 +0300

    pushing exrcise 8

---
+++ execise 8/main.js
@@ -0,0 +1,32 @@
+//  1: forEach
+// let numbers=[10,20,30,40,50]
+//   numbers.forEach(function(Number){
+//     console.log(Number)
+//   })
+
+// 2. map
+
+// let numbers=[2,4,6,8]
+
+//   let newNumbers = numbers.map(function(number){
+//     return number *2;
+//   })
+//   console.log(newNumbers)
+
+// 3.filter
+
+ let numbers=[10,15,20,25,30]
+
+  let newNumbers = numbers.filter(function(number){
+    return number > 20;
+  })
+  console.log(newNumbers)
+
+// 4: reduce
+// let numbers =[5,10,15,20]
+//   let total = numbers.reduce(function(sum,num){
+//     return sum+num},0)
+//     console.log(total)
+
+
+   
