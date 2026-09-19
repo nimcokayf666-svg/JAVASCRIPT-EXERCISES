0fed38ba5f67f9baed32b8292489e593298c6c98
Nimco <nimcokayf666@gmail.com>
2026-09-19 08:53:16 +0300

    pushing execise7

---
+++ execise 7/main.js
@@ -0,0 +1,39 @@
+// let fruits=["apple","Banana","mango","limo","orange"]
+// // fruits.push("yanyo")  
+// // fruits.pop()
+// // fruits.unshift("cambe")
+// // fruits.shift()
+
+// console.log(fruits.length)
+
+// 1.fruits
+
+let fruits=["apple","Banana","mango","limo","orange"]
+   console.log(fruits)
+
+// 2.student arrey
+
+let Students=["sihaam","nimco","burhaan","aamin","kayf"]
+ console.log(Students)
+
+// 3.studentinfo arrey
+let studentinfo ={name:"Nimco",age:21,faculty:"Software Engineering"
+
+};
+
+console.log(studentinfo.name);
+
+// 4.car
+let car={brand: "tayota",model: "corola",year:"2022"
+    
+};
+
+console.log(car.model)
+
+// 5.
+let student={ name: "kayf",age: 22,subject:["javascript", "css", "html"]
+
+};
+
+console.log(student.subject[1]);
+
